eae2a75b05be1c03ac50dd2ea168ae0023ea7d1e
Jani <jani.silva@slalom.com>
2026-05-22 17:09:49 -0400

    feat: integrate Vuetify components and directives in main.ts; remove unused plugins from vite.config.ts; add metadata and package.json for Vite dependencies

--- my-vue-app/src/main.ts
+++ my-vue-app/src/main.ts
@@ -4,12 +4,16 @@ import './assets/main.css'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const vuetify = createVuetify({
+  components,
+  directives,
   icons: {
     defaultSet: 'mdi',
   },
